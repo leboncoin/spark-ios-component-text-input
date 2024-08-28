fb17ede8a95d8964f92e8ba40c77ab3f3ccdc67e
Robin <lemaire.robin@icloud.com>
2024-08-28 15:49:04 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,101 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+// swiftlint:disable all
+let package = Package(
+    name: "Spark___COMPONENT_NAME___",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "Spark___COMPONENT_NAME___",
+            targets: ["Spark___COMPONENT_NAME___"]
+        ),
+        .library(
+            name: "Spark___COMPONENT_NAME___Testing",
+            targets: ["Spark___COMPONENT_NAME___Testing"]
+        )
+    ],
+    dependencies: [
+       .package(
+           url: "https://github.com/adevinta/spark-ios-common.git",
+           // path: "../spark-ios-common"
+           /*version*/ "0.0.1"..."999.999.999"
+       ),
+       .package(
+           url: "https://github.com/adevinta/spark-ios-theming.git",
+           // path: "../spark-ios-theming"
+           /*version*/ "0.0.1"..."999.999.999"
+       )
+    ],
+    targets: [
+        .target(
+            name: "Spark___COMPONENT_NAME___",
+            dependencies: [
+                .product(
+                    name: "SparkCommon",
+                    package: "spark-ios-common"
+                ),
+                .product(
+                    name: "SparkTheming",
+                    package: "spark-ios-theming"
+                )
+            ],
+            path: "Sources/Core"
+        ),
+        .target(
+            name: "Spark___COMPONENT_NAME___Testing",
+            dependencies: [
+                "Spark___COMPONENT_NAME___",
+                .product(
+                    name: "SparkCommon",
+                    package: "spark-ios-common"
+                ),
+                .product(
+                    name: "SparkCommonTesting",
+                    package: "spark-ios-common"
+                ),
+                .product(
+                    name: "SparkThemingTesting",
+                    package: "spark-ios-theming"
+                ),
+                .product(
+                    name: "SparkTheme",
+                    package: "spark-ios-theming"
+                )
+            ],
+            path: "Sources/Testing"
+        ),
+        .testTarget(
+            name: "Spark___COMPONENT_NAME___UnitTests",
+            dependencies: [
+                "Spark___COMPONENT_NAME___",
+                "Spark___COMPONENT_NAME___Testing",
+                .product(
+                    name: "SparkCommonTesting",
+                    package: "spark-ios-common"
+                ),
+                .product(
+                    name: "SparkThemingTesting",
+                    package: "spark-ios-theming"
+                )
+            ],
+            path: "Tests/UnitTests"
+        ),
+        .testTarget(
+            name: "Spark___COMPONENT_NAME___SnapshotTests",
+            dependencies: [
+                "Spark___COMPONENT_NAME___",
+                "Spark___COMPONENT_NAME___Testing",
+                .product(
+                    name: "SparkCommonSnapshotTesting",
+                    package: "spark-ios-common"
+                ),
+            ],
+            path: "Tests/SnapshotTests"
+        ),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,84 @@
+
+# ___COMPONENT_NAME___
+
+TODO: 
+
+## Specifications
+
+The ___component_name___ specifications on Zeroheight is [here](TODO:).
+
+![Figma anatomy](https://github.com/adevinta/___REPOSITORY_NAME___/blob/main/.github/assets/anatomy.png)
+
+## Usage
+
+___COMPONENT_NAME___ is available both in UIKit and SwiftUI.
+
+### UIKit
+
+### Usage
+
+#### Subviews
+
+* `TODO`: TODO.
+
+#### Properties
+
+* `TODO`: TODO.
+
+#### Published Properties
+
+* `TODO`: TODO.
+
+#### Initialization
+
+```swift
+let ___component_name___ = ___COMPONENT_NAME___UIView(
+    TODO: TODO
+)
+```
+
+#### Getter / Setter
+
+TODO
+
+
+### SwiftUI
+
+#### Initialization
+
+```swift
+let ___component_name___ = ___COMPONENT_NAME___View(
+    TODO: TODO
+)
+```
+
+#### Modifier
+
+TODO
+
+
+## License
+
+```
+MIT License
+
+Copyright (c) 2024 Adevinta
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
