24db66cc91d30dde5245d6b1c747850343ac7861
Robin Lemaire <robin.lemaire@adevinta.com>
2024-10-16 17:27:06 +0200

    [TextInput-1087] Add UIKit version

---
+++ Sources/Core/TextEditor/AccessibilityIdentifier/TextEditorAccessibiltyIdentifier.swift
@@ -0,0 +1,16 @@
+//
+//  TextEditorAccessibiltyIdentifier.swift
+//  SparkEditor
+//
+//  Created by alican.aycil on 29.05.24.
+//  Copyright © 2024 Adevinta. All rights reserved.
+//
+
+import Foundation
+
+/// The textview identifiers for the textfield.
+public enum TextEditorAccessibilityIdentifier {
+
+    /// The textview accessibility identifier.
+    public static let view = "spark-texteditor"
+}
---
+++ Sources/Core/TextEditor/Enum/TextEditorIntent.swift
@@ -0,0 +1,9 @@
+//
+//  TextEditorIntent.swift
+//  Spark
+//
+//  Created by Quentin.richard on 21/09/2023.
+//  Copyright © 2023 Adevinta. All rights reserved.
+//
+
+public typealias TextEditorIntent = TextInputIntent
